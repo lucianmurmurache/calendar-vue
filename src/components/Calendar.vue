81a1f65919d414b244eaf18e679eeecba3311cb1
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2020-08-24 10:07:53 +0200

    First commit

---
+++ src/components/Calendar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="calendar">
+    <h1>{{ msg }}</h1>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calendar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/DateIndicator.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="date-indicator"></div>
+</template>
+
+<script>
+export default {
+    name: 'DateIndicator',
+    props: {
+        /* Props */
+    },
+    computed: {
+        /* selected month */
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
